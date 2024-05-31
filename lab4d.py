f9cf1f95fc5671a6eb39157c7048815729344064
Vizi Patel <vpatel325@myseneca.ca>
2024-05-31 19:47:49 -0400

    Lab 4

---
+++ lab4d.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# Strings 1
+
+str1 = 'Hello World!!'
+str2 = 'Seneca College'
+
+num1 = 1500
+num2 = 1.50
+
+def first_five(n):
+    return n[:5]
+
+def last_seven(n):
+    return n[-7:]
+
+def middle_number(num):
+    return str(num)[1:3]
+
+def first_three_last_three(n1, n2):
+    return n1[:3] + n2[-3:]
+
+
+if __name__ == '__main__':
+    print(first_five(str1))
+    print(first_five(str2))
+    print(last_seven(str1))
+    print(last_seven(str2))
+    print(middle_number(num1))
+    print(middle_number(num2))
+    print(first_three_last_three(str1, str2))
+    print(first_three_last_three(str2, str1))
